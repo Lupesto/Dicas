b82d035dfbea23da1275eaeb718585c3cea1c6a2
lupesto <lucasthe2@gmail.com>
2022-06-29 23:18:46 -0300

    general

--- Argumentos.java
+++ Argumentos.java
@@ -1,4 +1,4 @@
-/* Luis Gustavo Godoy Camargo */
+
 public class Argumentos{
 	public static void main(String[] arg){
 	System.out.println(" public class Argumentos\npublic static void main(String[] arg)");
